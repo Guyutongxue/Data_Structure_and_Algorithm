72811471f7bb90ef3cbeeab4ce36b80dbfb0f094
Guyutongxue <guyutongxue@163.com>
2020-10-05 11:23:36 +0800

    update code

--- 03QueueAndStack/3.StackBasicOpt.cpp
+++ 03QueueAndStack/3.StackBasicOpt.cpp
@@ -1,64 +1,42 @@
 #include <iostream>
-#include <cstring>
+#include <queue>
+#include <stack>
+#include <sstream>
 
-int num[1000010]{};
-bool used[1000010]{};
-bool put[1000010]{};
+std::ostringstream oss;
+std::queue<std::size_t> inList;
+std::queue<std::size_t> outList;
+std::stack<std::size_t> mainStack;
 
 int main() {
-    int n;
+    std::size_t n;
     std::cin >> n;
-    for (int i = 0; i < n; i++) {
-        std::cin >> num[i];
-        // out-of-range or duplicated
-        if (num[i] > n || num[i] <= 0 || used[num[i]]) {
+    for (int i = 1; i <= n; i++) {
+        inList.push(i);
+    }
+    for (int i = 1; i <= n; i++) {
+        int value;
+        std::cin >> value;
+        if (value > n || value < 1) {
             std::cout << "NO" << std::endl;
             std::exit(0);
         }
-        used[num[i]] = true;
+        outList.push(value);
     }
-    std::memset(used, 0, sizeof(used));
-
-    for (int i = 0; i < n; i++) {
-        int m1 = num[i], m2 = num[i];
-        for (int j = i + 1; j < n; ++j) {
-            if (num[j] < m2) {
-                m2 = num[j];
-            } else if (num[j] < m1) {
+    while (!outList.empty()) {
+        if (mainStack.empty() || mainStack.top() != outList.front()) {
+            if (inList.empty()) {
                 std::cout << "NO" << std::endl;
                 std::exit(0);
             }
+            oss << "PUSH " << inList.front() << std::endl;
+            mainStack.push(inList.front());
+            inList.pop();
+        } else {
+            oss << "POP " << mainStack.top() << std::endl;
+            mainStack.pop();
+            outList.pop();
         }
     }
-    int cur{0};
-    for (int i = 0; i < n; i++) {
-        if (cur < num[i]) {
-            for (int j = cur + 1; j < num[i]; j++) {
-                if (!used[j]) {
-                    if (!put[j]) {
-                        std::cout << "PUSH " << j << std::endl;
-                        put[j] = true;
-                    }
-                    cur = j;
-                }
-            }
-            used[num[i]] = true;
-            if (!put[num[i]]) {
-                std::cout << "PUSH " << num[i] << std::endl;
-                put[num[i]] = true;
-            }
-            std::cout << "POP " << num[i] << std::endl;
-        } else if (cur == num[i]) {
-            used[cur] = true;
-            if (!put[num[i]]) {
-                std::cout << "PUSH " << num[i] << std::endl;
-                put[num[i]] = true;
-            }
-            std::cout << "POP " << num[i] << std::endl;
-            for (int j = cur; j >= 0; j--) {
-                if (!used[j])
-                    cur = j;
-            }
-        }
-    }
+    std::cout << oss.str();
 }
