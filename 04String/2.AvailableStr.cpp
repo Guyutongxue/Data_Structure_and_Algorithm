294622fc70c20a725f4b3cec135973cac91f61a2
Guyutongxue <guyutongxue@163.com>
2020-10-18 13:37:36 +0800

    fix style

--- 04String/2.AvailableStr.cpp
+++ 04String/2.AvailableStr.cpp
@@ -1,7 +1,6 @@
 #include <iostream>
 #include <regex>
 #include <string>
-#include <tuple>
 
 std::string ans[52];
 
@@ -13,7 +12,7 @@ int main() {
     }
     std::string pattern;
     std::cin >> pattern;
-    std::regex reg(pattern,std::regex_constants::icase);
+    std::regex reg(pattern, std::regex_constants::icase);
     for (int i = 1; i <= n; i++) {
         if (std::regex_match(ans[i], reg)) {
             std::cout << i << " " << ans[i] << std::endl;
